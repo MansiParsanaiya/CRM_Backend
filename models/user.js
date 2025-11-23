77b432ee20f1405918fc4c25b5611fa0f1b26a2e
MansiParsaniya <parsaniyamansi8141@gmail.com>
2025-11-23 14:30:54 +0530

    package upgradations

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   role: { type: String, enum: ['user', 'admin'] },
   isActive: { type: String, enum: ['pending', 'approved'], default: 'pending' },
-  userType: { type: String, enum: ['student', 'employee'], default: null }
+  userType: { type: String, default: null }
 });
 
 userSchema.plugin(mongoosePaginate);
